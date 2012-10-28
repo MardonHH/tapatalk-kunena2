d2d093d0b910b5c1da75f23e0add21ea38144cac
TomWu <phperwuhan@gmail.com>
2012-10-28 12:06:00 +0800

    add mark_read flag
    Signed-off-by: TomWu <phperwuhan@gmail.com>

--- mobiquo/custom/customConfig.php
+++ mobiquo/custom/customConfig.php
@@ -25,5 +25,6 @@ MbqMain::$customConfig['forum']['can_unread'] = MbqBaseFdt::getFdt('MbqFdtConfig
 MbqMain::$customConfig['forum']['subscribe_forum'] = MbqBaseFdt::getFdt('MbqFdtConfig.forum.subscribe_forum.range.support');
 MbqMain::$customConfig['forum']['subscribe_load'] = MbqBaseFdt::getFdt('MbqFdtConfig.forum.subscribe_load.range.support');
 MbqMain::$customConfig['forum']['report_post'] = MbqBaseFdt::getFdt('MbqFdtConfig.forum.report_post.range.support');
+MbqMain::$customConfig['forum']['mark_read'] = MbqBaseFdt::getFdt('MbqFdtConfig.forum.mark_read.range.support');
 
 ?>
